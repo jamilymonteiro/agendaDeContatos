9b038990b6747e9eb681bfa676a9e88f797b2966
Jamily Monteiro <jamilymonteiro57@gmail.com>
2025-05-26 12:47:07 -0300

    update adicionar contato - Jamily

---
+++ src/app/adicionar/adicionar.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar',
+  templateUrl: './adicionar.page.html',
+  styleUrls: ['./adicionar.page.scss'],
+  standalone: false,
+})
+export class AdicionarPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
