cdb267b7f60344478def2f524d4206ac0adc0862
Kira175 <167375077+Kira175@users.noreply.github.com>
2025-06-01 21:41:22 -0300

    Update tab2.page.ts

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContatosFavoritosService } from 'src/app/services/contatos-favoritos.service';
 
 @Component({
   selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss'],
-  standalone: false,
+  templateUrl: './tab2.page.html',
+  styleUrls: ['./tab2.page.scss'],
+  standalone: false  // ← Importante!
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  favoritos: any[] = [];
 
-  constructor() {}
+  constructor(private favoritosService: ContatosFavoritosService) {}
 
+  ngOnInit() {
+    this.favoritosService.favoritos$.subscribe((favoritos: any[]) => {
+      this.favoritos = favoritos;
+    });
+  }
+
+  removerFavorito(nome: string) {
+    this.favoritosService.removerFavorito(nome);
+  }
 }
