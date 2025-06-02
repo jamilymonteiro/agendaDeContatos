1b1bfc8f7aec60534424d7d4ff1aa2cb8602e929
Kira175 <167375077+Kira175@users.noreply.github.com>
2025-06-01 21:41:11 -0300

    Update tab2.page.spec.ts

--- src/app/tab2/tab2.page.spec.ts
+++ src/app/tab2/tab2.page.spec.ts
@@ -1,22 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab2Page } from './tab2.page';
+import { ContatosFavoritosService } from 'src/app/services/contatos-favoritos.service';
 
 describe('Tab2Page', () => {
   let component: Tab2Page;
   let fixture: ComponentFixture<Tab2Page>;
+  let favoritosService: ContatosFavoritosService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [Tab2Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+      imports: [IonicModule.forRoot(), ExploreContainerComponentModule],
+      providers: [ContatosFavoritosService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Tab2Page);
     component = fixture.componentInstance;
+    favoritosService = TestBed.inject(ContatosFavoritosService);
     fixture.detectChanges();
   });
 
